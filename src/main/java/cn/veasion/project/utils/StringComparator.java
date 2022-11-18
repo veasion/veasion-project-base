351a140ecbe6092bd432f4440bd3716477bbf01b
veasion <veasion@qq.com>
2022-11-18 15:47:53 +0800

    优化字符串排序

--- src/main/java/cn/veasion/project/utils/StringComparator.java
+++ src/main/java/cn/veasion/project/utils/StringComparator.java
@@ -1,6 +1,8 @@
 package cn.veasion.project.utils;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * StringComparator
@@ -10,20 +12,44 @@ import java.util.Comparator;
  */
 public class StringComparator implements Comparator<String> {
 
-    private String str1, str2;
+    private char[] str1, str2;
     private int pos1, pos2, len1, len2;
+    private static final Map<Character, Character> convertMap;
+
+    static {
+        convertMap = new HashMap<>();
+        convertMap.put('零', '0');
+        convertMap.put('一', '1');
+        convertMap.put('二', '2');
+        convertMap.put('三', '3');
+        convertMap.put('四', '4');
+        convertMap.put('五', '5');
+        convertMap.put('六', '6');
+        convertMap.put('七', '7');
+        convertMap.put('八', '8');
+        convertMap.put('九', '9');
+    }
+
+    public static void addConvert(Character a, Character b) {
+        convertMap.put(a, b);
+    }
 
     @Override
     public int compare(String s1, String s2) {
-        str1 = s1 == null ? "" : s1;
-        str2 = s2 == null ? "" : s2;
-        len1 = str1.length();
-        len2 = str2.length();
+        if (s1 == null) {
+            return -1;
+        } else if (s2 == null) {
+            return 1;
+        }
+        str1 = toChars(s1);
+        str2 = toChars(s2);
+        len1 = str1.length;
+        len2 = str2.length;
         pos1 = pos2 = 0;
         int result = 0;
         while (result == 0 && pos1 < len1 && pos2 < len2) {
-            char ch1 = str1.charAt(pos1);
-            char ch2 = str2.charAt(pos2);
+            char ch1 = str1[pos1];
+            char ch2 = str2[pos2];
             if (Character.isDigit(ch1)) {
                 result = Character.isDigit(ch2) ? compareNumbers() : -1;
             } else if (Character.isLetter(ch1)) {
@@ -39,21 +65,30 @@ public class StringComparator implements Comparator<String> {
         return result == 0 ? len1 - len2 : result;
     }
 
+    private char[] toChars(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            chars[i] = convertMap.getOrDefault(c, c);
+        }
+        return chars;
+    }
+
     private int compareNumbers() {
         int end1 = pos1 + 1;
-        while (end1 < len1 && Character.isDigit(str1.charAt(end1))) {
+        while (end1 < len1 && Character.isDigit(str1[end1])) {
             end1++;
         }
         int fullLen1 = end1 - pos1;
-        while (pos1 < end1 && str1.charAt(pos1) == '0') {
+        while (pos1 < end1 && str1[pos1] == '0') {
             pos1++;
         }
         int end2 = pos2 + 1;
-        while (end2 < len2 && Character.isDigit(str2.charAt(end2))) {
+        while (end2 < len2 && Character.isDigit(str2[end2])) {
             end2++;
         }
         int fullLen2 = end2 - pos2;
-        while (pos2 < end2 && str2.charAt(pos2) == '0') {
+        while (pos2 < end2 && str2[pos2] == '0') {
             pos2++;
         }
         int delta = (end1 - pos1) - (end2 - pos2);
@@ -61,7 +96,7 @@ public class StringComparator implements Comparator<String> {
             return delta;
         }
         while (pos1 < end1 && pos2 < end2) {
-            delta = str1.charAt(pos1++) - str2.charAt(pos2++);
+            delta = str1[pos1++] - str2[pos2++];
             if (delta != 0) {
                 return delta;
             }
@@ -72,8 +107,8 @@ public class StringComparator implements Comparator<String> {
     }
 
     private int compareOther(boolean isLetters) {
-        char ch1 = str1.charAt(pos1);
-        char ch2 = str2.charAt(pos2);
+        char ch1 = str1[pos1];
+        char ch2 = str2[pos2];
         if (ch1 == ch2) {
             return 0;
         }
